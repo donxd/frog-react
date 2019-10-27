73f87533a49824d699c862f44c8d90f4aa5c2cae
donxd <n0s3.xd@gmail.com>
2019-10-27 04:02:24 -0600

    feat(Frog): add preconfig number of boxes and selected element

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Frog from './Frog';
 
 class App extends Component {
   render () {
+    // <Frog></Frog>
+    // <Frog boxes="7" selected="4"></Frog>
+    // <Frog config selected="2"></Frog>
+    // <Frog boxes="10"></Frog>
+    // <Frog boxes="7" selected="4"></Frog>
+    // <Frog id='test_config' config></Frog>
+    
+    // <Frog boxes="7" selected="4"></Frog>
     return (
       <div>
           <Frog></Frog>
--- src/Frog.js
+++ src/Frog.js
@@ -18,32 +18,73 @@ class Frog extends Component {
     }
     componentDidMount () {
         console.log('componentDidMount');
-        this.generateObjectBoxes();
+        this.loadGenerateObjectBoxes();
+        // this.generateObjectBoxes();
     }
-    // componentWillReceiveProps (nextProps) {
-    //     console.log('componentWillReceiveProps - nextProps : ', JSON.stringify(nextProps));
-    // }
-    // shouldComponentUpdate (nextProps, nextState) {
-    //     console.log('shouldComponentUpdate - nextProps : ', JSON.stringify(nextProps));
-    //     console.log('shouldComponentUpdate - nextState : ', JSON.stringify(nextState));
-    //     return true;
-    // }
-    // componentWillUpdate (nextProps, nextState) {
-    //     console.log('componentWillUpdate - nextProps : ', JSON.stringify(nextProps));
-    //     console.log('componentWillUpdate - nextState : ', JSON.stringify(nextState));
-    // }
-    generateObjectBoxes () {
-      this.boxesElements = [];
-      for (let i = 0; i < this.state.boxes; i++) {
-        const box = this.getNewBoxObject(i+1, this.state.selected);
+    loadGenerateObjectBoxes () {
+        this.generateObjectBoxes();
+        this.setState(Object.assign(this.state, {boxesElements: JSON.parse(JSON.stringify(this.boxesElements))}));
+    }
+    componentWillReceiveProps (nextProps) {
+        console.log('1 componentWillReceiveProps - nextProps : ', JSON.stringify(nextProps));
+    }
+    shouldComponentUpdate (nextProps, nextState) {
+        console.log('2 shouldComponentUpdate - nextProps : ', JSON.stringify(nextProps));
+        console.log('2 shouldComponentUpdate - nextState : ', JSON.stringify(nextState));
+        console.log('2 shouldComponentUpdate - boxesElementsPrev : ', JSON.stringify(this.boxesElementsPrev));
+        if ((this.boxesElementsPrev === undefined) || this.isChangeToApply(nextProps, nextState)) {
+            this.generateObjectBoxes(nextProps.boxes, nextProps.selected);
+            // if (JSON.stringify(this.boxesElementsPrev) !== JSON.stringify(nextState.boxesElements)) {
+            if (
+                // ('{}' === JSON.stringify(nextProps)) || 
+                // (nextProps.boxes === undefined && nextProps.selected === undefined) 
+                // (this.boxesElementsPrev === undefined) 
+                // ||
+                (nextProps.boxes !== undefined && nextState.boxes !== undefined && nextProps.boxes !== nextState.boxes) || 
+                (nextProps.selected !== undefined && nextState.selected !== undefined && nextProps.selected !== nextState.selected)) {
+                nextState.boxesElements = JSON.parse(JSON.stringify(this.boxesElements));
+            }
+            this.setState(Object.assign(nextState, {...nextProps}));
+            this.boxesElementsPrev = JSON.parse(JSON.stringify(nextState.boxesElements));
+            // this.boxesElementsPrev = [...nextState.boxesElements];
+            return true;
+        }
 
-        this.boxesElements.push(box);
-      }
-      this.setState(Object.assign(this.state, {boxesElements: this.boxesElements}));
+        return false;
+    }
+    isChangeToApply (nextProps, nextState) {
+        console.log('2 isChangeToApply # boxes    : ', (nextProps.boxes !== undefined && nextState.boxes !== undefined && nextProps.boxes !== nextState.boxes));
+        console.log('2 isChangeToApply # selected : ', (nextProps.selected !== undefined && nextState.selected !== undefined && nextProps.selected !== nextState.selected));
+        console.log('2 isChangeToApply # config   : ', (nextProps.config !== undefined && nextState.config !== undefined && nextProps.config !== nextState.config));
+        console.log('2 isChangeToApply # bA       : ', JSON.stringify(this.boxesElementsPrev));
+        console.log('2 isChangeToApply # bB       : ', JSON.stringify(nextState.boxesElements));
+        console.log('2 isChangeToApply # boxesE   : ', (this.boxesElementsPrev !== undefined && nextState.boxesElements !== undefined && JSON.stringify(this.boxesElementsPrev) !== JSON.stringify(nextState.boxesElements)));
+        return (nextProps.boxes !== undefined && nextState.boxes !== undefined && nextProps.boxes !== nextState.boxes) ||
+            (nextProps.selected !== undefined && nextState.selected !== undefined && nextProps.selected !== nextState.selected) ||
+            (nextProps.config !== undefined && nextState.config !== undefined && nextProps.config !== nextState.config) ||
+            // (this.boxesElementsPrev !== undefined && nextState.boxesElements !== undefined && this.boxesElementsPrev !== nextState.boxesElements);
+            (this.boxesElementsPrev !== undefined && nextState.boxesElements !== undefined && JSON.stringify(this.boxesElementsPrev) !== JSON.stringify(nextState.boxesElements));
+    }
+    componentWillUpdate (nextProps, nextState) {
+        console.log('3 componentWillUpdate - nextProps : ', JSON.stringify(nextProps));
+        console.log('3 componentWillUpdate - nextState : ', JSON.stringify(nextState));
+    }
+    generateObjectBoxes (nBoxes=null, selectedBox=null) {
+        console.log(`nBoxes : ${nBoxes} ### selectedBox : ${selectedBox}`);
+        const numberBoxes = nBoxes !== null ? nBoxes : this.state.boxes;
+        // const boxSelected = selectedBox !== null ? selectedBox-1 : this.state.selected;
+        const boxSelected = selectedBox !== null ? selectedBox : this.state.selected;
+        this.boxesElements = [];
+        for (let i = 0; i < numberBoxes; i++) {
+            const box = this.getNewBoxObject(i+1, boxSelected);
+
+            this.boxesElements.push(box);
+        }
+      //this.setState(Object.assign(this.state, {boxesElements: this.boxesElements})); //*
       // this.setState({boxesElements: this.boxesElements});
     }
     getNewBoxObject (index, itemSelected) {
-      const attrSelected = index === itemSelected;
+      const attrSelected = index === Number.parseInt(itemSelected);
 
       return this.getBoxObject(index, attrSelected);
     }
@@ -60,17 +101,24 @@ class Frog extends Component {
       return item.selected ? CSS_STYLE_FROG : '';
     }
     jumpFrogAdv (boxSelected) {
+        console.log('jumpFrogAdv - boxSelected ', JSON.stringify(boxSelected));
         if (boxSelected.selected) {
             const newBoxSelection = boxSelected.index < this.state.boxesElements.length ?
                 this.state.boxesElements[ boxSelected.index ] :
                 this.state.boxesElements[ 0 ];
 
-            this.boxesElements = this.state.boxesElements;
-            this.boxesElements[boxSelected.index-1].selected = false;
-            this.boxesElements[newBoxSelection.index-1].selected = true;
+            // let boxesElements = this.state.boxesElements;
+            // let boxesElements = [...this.state.boxesElements];
+            let boxesElements = JSON.parse(JSON.stringify(this.state.boxesElements));
+            this.boxesElementsPrev = JSON.parse(JSON.stringify(boxesElements));
+            console.log('jumpFrogAdv - prev boxesElements ', JSON.stringify(this.boxesElementsPrev));
+            boxesElements[boxSelected.index-1].selected = false;
+            boxesElements[newBoxSelection.index-1].selected = true;
+            console.log('jumpFrogAdv - next boxesElements ', JSON.stringify(boxesElements));
+            console.log('jumpFrogAdv * prev boxesElements ', JSON.stringify(this.boxesElementsPrev));
 
-            this.setState(Object.assign(this.state, {boxesElements: this.boxesElements}));
-            // this.boxesElements = [...this.boxesElements];
+            this.setState(Object.assign(this.state, {boxesElements: boxesElements}));
+            // boxesElements = [...boxesElements];
 
             // this.requestUpdate();
         }
