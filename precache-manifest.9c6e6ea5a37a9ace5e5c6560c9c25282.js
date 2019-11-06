e659f8625d2b22408706adb986f80f6d47fda0a4
donxd <n0s3.xd@gmail.com>
2019-11-06 14:01:20 -0600

    Updates

---
+++ precache-manifest.9c6e6ea5a37a9ace5e5c6560c9c25282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3448b2e22f6d4734de84894714eb43f9",
+    "url": "/frog-react/index.html"
+  },
+  {
+    "revision": "bde2f9be97d03b3d56e6",
+    "url": "/frog-react/static/css/main.d0b17853.chunk.css"
+  },
+  {
+    "revision": "deddacc18db8a9752366",
+    "url": "/frog-react/static/js/2.63cc6f62.chunk.js"
+  },
+  {
+    "revision": "bde2f9be97d03b3d56e6",
+    "url": "/frog-react/static/js/main.3404201e.chunk.js"
+  },
+  {
+    "revision": "3ce685bc5ee643c7e800",
+    "url": "/frog-react/static/js/runtime-main.9bab9afd.js"
+  },
+  {
+    "revision": "d0d6331bae364e6923c084cc1c5d9db6",
+    "url": "/frog-react/static/media/frog.d0d6331b.gif"
+  }
+]);
